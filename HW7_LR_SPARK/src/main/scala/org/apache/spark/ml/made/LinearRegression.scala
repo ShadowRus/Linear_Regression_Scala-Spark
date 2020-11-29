45e4740ad4f5f34fe0fec0cb1c0ce3729e10f550
ShadowRus <51329746+ShadowRus@users.noreply.github.com>
2020-11-30 00:23:55 +0300

    Update LinearRegression.scala

--- HW7_LR_SPARK/src/main/scala/org/apache/spark/ml/made/LinearRegression.scala
+++ HW7_LR_SPARK/src/main/scala/org/apache/spark/ml/made/LinearRegression.scala
@@ -1,6 +1,6 @@
 package org.apache.spark.ml.made
 
-import breeze.linalg.{sum, DenseVector => BVector}
+import breeze.linalg.{sum, DenseVector}
 import org.apache.spark.ml.linalg.{DenseVector, Vector, Vectors}
 import org.apache.spark.ml.regression.{RegressionModel}
 import org.apache.spark.ml.{Estimator,Model,PredictorParams}
@@ -17,8 +17,8 @@ trait LinearRegressionParams extends HasFeaturesCol with HasLabelCol with HasPre
   final val learningRate: DoubleParam = new DoubleParam(this, "learningRate", "learning rate")
   final val : IntParam = new IntParam(this, "Epochs", "number of iters")
 
-  setDefault(learningRate, 1.0)
-  setDefault(Epochs, 100)
+  setDefault(learningRate, 0.003)
+  setDefault(Epochs, 3000)
 
   def setLearningRate(value: Double): this.type = set(learningRate, value)
   def setEpochs(value: Int): this.type = set(Epochs, value)
@@ -37,25 +37,25 @@ class LinearRegression(override val uid: String)
 
   override protected def train(dataset: Dataset[_]): LinearRegressionModel = {
     val numFeatures = MetadataUtils.getNumFeatures(dataset, $(featuresCol))
-    var weights: BVector[Double] = BVector.zeros(numFeatures + 1)
+    var weights: DenseVector[Double] = DenseVector.zeros(numFeatures + 1)
     val gradCol = "grad"
 
     val transform = dataset.sqlContext.udf.register(uid + "_grad",
       (x_no_ones: Vector, y: Double) => {
-        val one = BVector(1.0)
-        val x = BVector.vertcat(one, x_no_ones.asBreeze.toDenseVector)
+        val one = DenseVector(1.0)
+        val x = DenseVector.vertcat(one, x_no_ones.asBreeze.toDenseVector)
         val grad = x * (sum(x * weights) - y)
         Vectors.fromBreeze(grad)
       }
     )
 
     for (_ <- 0 to $(Epochs)) {
-      val dataset_transformed = dataset.withColumn(gradCol, transform(dataset($(featuresCol)), dataset($(labelCol))))
-      val Row(Row(grad_mean_arr)) = dataset_transformed
-        .select(Summarizer.metrics("mean").summary(dataset_transformed(gradCol)))
+      val dataset_transform = dataset.withColumn(gradCol, transform(dataset($(featuresCol)), dataset($(labelCol))))
+      val Row(Row(grad_mean_arr)) = dataset_transform
+        .select(Summarizer.metrics("mean").summary(dataset_transform(gradCol)))
         .first()
 
-      val grad_mean: BVector[Double] = grad_mean_arr.asInstanceOf[DenseVector].asBreeze.toDenseVector
+      val grad_mean: DenseVector[Double] = grad_mean_arr.asInstanceOf[DenseVector].asBreeze.toDenseVector
       weights = weights - $(learningRate) * grad_mean
     }
     val params = Vectors.fromBreeze(weights)
@@ -73,14 +73,14 @@ class LinearRegressionModel protected[made](override val uid: String, weights: V
   def this(weights: Vector) = this(Identifiable.randomUID("linearRegressionModel"), weights)
 
   override def predict(features: Vector): Double = {
-    val one = BVector(1.0)
-    val x = BVector.vertcat(one, features.asBreeze.toDenseVector)
+    val one =  DenseVector(1.0)
+    val x =  DenseVector.vertcat(one, features.asBreeze.toDenseVector)
     sum(x * weights.asBreeze.toDenseVector)
   }
 
   override def copy(extra: ParamMap): LinearRegressionModel = copyValues(new LinearRegressionModel(weights))
 
-  def getWeights(): BVector[Double] = {
+  def getWeights():  DenseVector[Double] = {
     weights.asBreeze.toDenseVector
   }
 
